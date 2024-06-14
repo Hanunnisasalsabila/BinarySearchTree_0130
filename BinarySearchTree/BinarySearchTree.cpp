5b7f275e6e1c51b35c448e4248464dc61efea6b2
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-06-14 11:18:51 +0700

    Sedangkan currentNode bukan NULL dan nilai di kolom data currentNode tidak sama dengan nilai di kolom data node baru

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -50,5 +50,9 @@ public:
 	void search(string element, Node*& parent, Node*& currentNode) {
 		currentNode = ROOT;
 		parent = NULL;
+		
+		while ((currentNode != NULL) && (currentNode->info != element)) {
+
+		}
 	}
 };
