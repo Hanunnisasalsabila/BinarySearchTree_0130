4b017e815c37cf613ca1bd7777fe9b235b8bde47
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-06-14 11:07:03 +0700

    cari node yang akan menjadi orang tua dari node baru

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -27,5 +27,10 @@ public:
 		newNode->info = element;
 		newNode->leftchild = nullptr;
 		newNode->rightchild = nullptr;
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+
+		search(element, parent, currentNode);
 	}
 };
